62056f3b6827fa005b2f80d2b104ae682985f307
ByEllenn <sousa.ellen@escolar.ifrn.edu.br>
2024-09-09 07:46:35 -0300

    Add files via upload

---
+++ TESTE.php
@@ -0,0 +1,13 @@
+<?php
+// Exibindo uma mensagem simples
+echo "Olá, mundo!";
+
+// Exibindo a data e hora atual
+echo "<br>Data e Hora Atual: " . date('d/m/Y H:i:s');
+
+// Realizando uma operação matemática simples
+$numero1 = 10;
+$numero2 = 20;
+$soma = $numero1 + $numero2;
+echo "<br>A soma de $numero1 e $numero2 é: $soma";
+?>
